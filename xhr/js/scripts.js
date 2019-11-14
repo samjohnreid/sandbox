09acd3e58e85224505ceb4b6cac51d6d7d0eb9fd
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2019-11-14 18:05:58 -0500

    adding fetch

--- xhr/js/scripts.js
+++ xhr/js/scripts.js
@@ -1,11 +1,51 @@
-const container = document.getElementById('dynamic-data');
+// const container = document.getElementById('dynamic-data');
 
-let xhr = new XMLHttpRequest();
+// let xhr = new XMLHttpRequest();
 
-xhr.open('GET', 'http://time.jsontest.com', true);
+// xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
 
-xhr.send();
+// xhr.send();
 
-xhr.onload = () => {
-  container.innerHTML = xhr.response;
-}
+// xhr.onload = () => {
+//   container.innerHTML = xhr.response;
+// }
+
+// console.log(xhr);
+
+// this is with XMHHttpRequest...
+
+// function reqListener() {
+//   var data = JSON.parse(this.responseText);
+//   console.log(data);
+// }
+
+// function reqError() {
+//   console.log('Fetch Error :-S', err);
+// }
+
+// var oReq = new XMLHttpRequest();
+// oReq.onload = reqListener;
+// oReq.onerror = reqError;
+// oReq.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
+// oReq.send();
+
+// ... now with Fetch!
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
+
