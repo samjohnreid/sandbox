c34ff56e56b6dcf1b4a36b1e4631a879dd46db9f
Samuel Reid <sreid2@Samuels-MBP.duke-energy.com>
2019-12-12 18:05:43 -0500

    added view portion

--- mvc/js/scripts.js
+++ mvc/js/scripts.js
@@ -4,8 +4,16 @@ class Model {
   constructor() {
     // The state of the model, an array of todo objects, prepopulated with some data
     this.todos = [
-      {id: 1, text: 'Run a marathon', complete: false},
-      {id: 2, text: 'Plant a garden', complete: false}
+      {
+        id: 1,
+        text: 'Run a marathon',
+        complete: false
+      },
+      {
+        id: 2,
+        text: 'Plant a garden',
+        complete: false
+      }
     ];
   }
 
@@ -44,7 +52,105 @@ class Model {
 // ******************** VIEW ********************
 
 class View {
-  constructor() {}
+  constructor() {
+    // The root element
+    this.app = this.getElement('#root');
+
+    // The title of the app
+    this.title = this.createElement('h1');
+    this.title.textContent = 'Todos';
+
+    // The form, with a [type="text"] input, and a submit button
+    this.form = this.createElement('form');
+
+    this.input = this.createElement('input');
+    this.input.type = 'text';
+    this.input.placeholder = 'Add todo';
+    this.input.name = 'todo';
+
+    this.submitButton = this.createElement('button');
+    this.submitButton.textContent = 'Submit';
+
+    // The visual representation of the todo list
+    this.todoList = this.createElement('ul', 'todo-list');
+
+    // Append the input and submit button to the form
+    this.form.append(this.input, this.submitButton);
+
+    // Append the title, form and todo list to the app
+    this.app.append(this.title, this.form, this.todoList);
+  }
+
+  get _todoText() {
+    return this.input.value;
+  }
+
+  _resetInput() {
+    this.input.value = '';
+  }
+
+  displayTodos(todos) {
+    // Delete all nodes
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild);
+    }
+
+    // Show default message
+    if (todos.length === 0) {
+      const p = this.createElement('p');
+      p.textContent = 'Nothing to do! Add a task?';
+      this.todoList.append(p);
+    } else {
+      // Create todo item nodes for each todo in state
+      todos.forEach(todo => {
+        const li = this.createElement('li');
+        li.id = todo.id;
+
+        // Each todo item will have a checkbox you can toggle
+        const checkbox = this.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = todo.complete;
+
+        // The todo item text will be in a contentEditable span
+        const span = createElement('span');
+        span.contentEditable = true;
+        span.classList.add('editable');
+
+        // If the todo is complete, it will have a strikethrough
+        if (todo.complete) {
+          const strike = this.createElement('s');
+          strike.textContent = todo.text;
+          span.append(strike);
+        } else {
+          // Otherwise just display the text
+          span.textContent = todo.text;
+        }
+
+        // The todos will also have a delete button
+        const deleteButton = this.createElement('button', 'delete');
+        deleteButton.textContent = 'Delete';
+        li.append(checkbox, span, deleteButton);
+
+        // Append nodes to the todo list
+        this.todoList.append(li);
+      });
+    }
+  }
+
+  // Create an element with an optional CSS class
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) {
+      element.classList.add(className);
+    }
+    return element;
+  }
+
+  // Retrieve an element from the DOM
+  getElement(selector) {
+    const element = document.querySelector(selector);
+    return element;
+  }
 }
 
 // ******************** CONTROLLER ********************
@@ -56,11 +162,11 @@ class Controller {
   }
 }
 
+// ********************
+
 const app = new Controller(new Model(), new View());
 
-
-
-// ********************  ********************
+// ********************
 
 // console.log(app.model.todos);
 
