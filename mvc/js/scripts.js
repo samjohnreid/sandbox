617089489ab3b5372b4d8b823ebb529636b80ed2
Samuel Reid <sreid2@Samuels-MBP.duke-energy.com>
2020-01-09 18:05:53 -0500

    finished mvc tutorial\!

--- mvc/js/scripts.js
+++ mvc/js/scripts.js
@@ -3,22 +3,16 @@
 class Model {
   constructor() {
     // The state of the model, an array of todo objects, prepopulated with some data
-    this.todos = [
-      {
-        id: 1,
-        text: 'Run a marathon',
-        complete: false
-      },
-      {
-        id: 2,
-        text: 'Plant a garden',
-        complete: false
-      }
-    ];
+    this.todos = JSON.parse(localStorage.getItem('todos') || [])
   }
 
   bindTodoListChanged(callback) {
-    this.onTodoListChanged = callback;
+    this.onTodoListChanged = callback
+  }
+
+  _commit(todos) {
+    this.onTodoListChanged(todos)
+    localStorage.setItem('todos', JSON.stringify(todos))
   }
 
   addTodo(todoText) {
@@ -26,37 +20,35 @@ class Model {
       id: this.todos.length > 0 ? this.todos[this.todos.length - 1].id + 1 : 1,
       text: todoText,
       complete: false
-    };
+    }
 
     this.todos.push(todo);
-    this.onTodoListChanged(this.todos)
+    this._commit(this.todos)
   }
 
   // Map through all todos, and replace the text of the todo with the specified id
   editTodo(id, updatedText) {
     this.todos = this.todos.map(todo =>
       todo.id === id ? {id: todo.id, text: updatedText, complete: todo.complete} : todo
-    );
+    )
 
-    this.onTodoListChanged(this.todos)
+    this._commit(this.todos)
   }
 
   // Filter a todo out of the array by id
   deleteTodo(id) {
-    this.todos = this.todos.filter(todo =>
-      todo.id !== id
-    );
+    this.todos = this.todos.filter(todo => todo.id !== id)
 
-    this.onTodoListChanged(this.todos)
+    this._commit(this.todos)
   }
 
   // Flip the complete boolean on the specified todo
   toggleTodo(id) {
     this.todos = this.todos.map(todo =>
       todo.id === id ? {id: todo.id, text: todo.text, complete: !todo.complete} : todo
-    );
+    )
 
-    this.onTodoListChanged(this.todos)
+    this._commit(this.todos)
   }
 }
 
@@ -91,29 +83,33 @@ class View {
 
     // Append the title, form and todo list to the app
     this.app.append(this.title, this.form, this.todoList);
+
+    this._temporaryTodoText = ''
+    this._initLocalListeners();
   }
 
   get _todoText() {
-    return this.input.value;
+    return this.input.value
   }
 
   _resetInput() {
-    this.input.value = '';
+    this.input.value = ''
   }
 
   // Create an element with an optional CSS class
   createElement(tag, className) {
-    const element = document.createElement(tag);
-    if (className) {
-      element.classList.add(className);
-    }
-    return element;
+    const element = document.createElement(tag)
+
+    if (className) element.classList.add(className)
+
+    return element
   }
 
   // Retrieve an element from the DOM
   getElement(selector) {
-    const element = document.querySelector(selector);
-    return element;
+    const element = document.querySelector(selector)
+    
+    return element
   }
 
   displayTodos(todos) {
@@ -139,7 +135,7 @@ class View {
         checkbox.checked = todo.complete;
 
         // The todo item text will be in a contentEditable span
-        const span = createElement('span');
+        const span = this.createElement('span');
         span.contentEditable = true;
         span.classList.add('editable');
 
@@ -162,6 +158,27 @@ class View {
         this.todoList.append(li);
       });
     }
+  }
+
+  // Update temporary state
+  _initLocalListeners() {
+    this.todoList.addEventListener('input', event => {
+      if (event.target.className === 'editable') {
+        this._temporaryTodoText = event.target.innerText
+      }
+    })
+  }
+
+  // Send the completed value to the model
+  bindEditTodo(handler) {
+    this.todoList.addEventListener('focusout', event => {
+      if (this._temporaryTodoText) {
+        const id = parseInt(event.target.parentElement.id)
+
+        handler(id, this._temporaryTodoText)
+        this._temporaryTodoText = ''
+      }
+    })
   }
 
   bindAddTodo(handler) {
@@ -201,38 +218,38 @@ class View {
 
 class Controller {
   constructor(model, view) {
-    this.model = model;
-    this.view = view;
+    this.model = model
+    this.view = view
 
-    this.model.bindTodoListChanged(this.onTodoListChanged);
+    this.model.bindTodoListChanged(this.onTodoListChanged)
     
-    this.view.bindAddTodo(this.handleAddTodo);
-    this.view.bindDeleteTodo(this.handleDeleteTodo);
-    this.view.bindToggleTodo(this.handleToggleTodo);
-    //this.view.bindEditTodo(this.handleEditTodo);
+    this.view.bindAddTodo(this.handleAddTodo)
+    this.view.bindDeleteTodo(this.handleDeleteTodo)
+    this.view.bindToggleTodo(this.handleToggleTodo)
+    this.view.bindEditTodo(this.handleEditTodo)
 
     // Display initial todos
-    this.onTodoListChanged(this.model.todos);
+    this.onTodoListChanged(this.model.todos)
   }
 
   onTodoListChanged = todos => {
-    this.view.displayTodos(todos);
+    this.view.displayTodos(todos)
   }
 
   handleAddTodo = todoText => {
-    this.model.addTodo(todoText);
+    this.model.addTodo(todoText)
   }
 
   handleEditTodo = (id, todoText) => {
-    this.model.editTodo(id, todoText);
+    this.model.editTodo(id, todoText)
   }
 
   handleDeleteTodo = id => {
-    this.model.deleteTodo(id);
+    this.model.deleteTodo(id)
   }
 
   handleToggleTodo = id => {
-    this.model.toggleTodo(id);
+    this.model.toggleTodo(id)
   }
 }
 
