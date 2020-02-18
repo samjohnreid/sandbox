97c67aee024e8b4db3881f6e02ee556bffbf1535
Samuel Reid <sreid2@Samuels-MBP.duke-energy.com>
2020-02-18 10:48:59 -0500

    more fiddling with IntersectionObserver

--- intersection-observer/js/scripts.js
+++ intersection-observer/js/scripts.js
@@ -1,11 +1,18 @@
-const stickySentinel = document.querySelector('.sticky-sentinel');
+// const stickySentinel = document.querySelector('.sticky-sentinel');
+const stickyPickle = document.querySelector('.pickle-ping');
+
+const options = {
+  threshold: 0
+};
 
 observer = new IntersectionObserver((entry) => {
-  if (!entry[0].isIntersecting && entry[0].boundingClientRect.top < 0) {
+  // if (!entry[0].isIntersecting && entry[0].boundingClientRect.top < 0) {
+  // console.log(entry[0]);
+  if (entry[0].isIntersecting) {
     console.log('add class');
   } else {
     console.log('remove class');
   }
-});
+}, options);
 
-observer.observe(stickySentinel);
+observer.observe(stickyPickle);
