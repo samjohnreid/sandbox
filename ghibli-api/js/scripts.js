36040e8476f6522d23dafde6d57b923d64f5f7f7
Samuel Reid <sreid2@samuels-mbp.duke-energy.com>
2020-02-19 18:20:56 -0500

    consuming data endpoint and dynamically building out html

--- ghibli-api/js/scripts.js
+++ ghibli-api/js/scripts.js
@@ -1,1 +1,45 @@
-alert('hello');
+// // Create a variable and assign a new XMLHttpRequest object to it
+// var request = new XMLHttpRequest();
+
+// // Open a new connection, using the GET request on the URL endpoint
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+
+// request.onload = function() {
+//   // Begin accessing JSON data here
+//   var data = JSON.parse(this.response);
+  
+//   data.forEach(movie => {
+//     console.log(movie.title)
+//     console.log(movie.description)
+//   });
+// }
+
+// // Send request
+// request.send();
+
+// **********************************************
+
+// Using Fetch instead of XMLHttpRequest!!
+fetch('https://ghibliapi.herokuapp.com/films')
+  .then(res => res.json())
+  .then(data => {
+    data.forEach(movie => {
+      console.log(movie.title);
+    });
+})
+.catch(function() {
+  console.log('error! :(');
+});
+
+// **********************************************
+
+const app = document.getElementById('root');
+
+const logo = document.createElement('img');
+logo.setAttribute('src', 'logo.png');
+
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+app.appendChild(logo);
+app.appendChild(container);
