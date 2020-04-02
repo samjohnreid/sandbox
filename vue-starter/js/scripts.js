0708901f9c894602a671b86e4d62519111f13e34
Samuel Reid <sam@Samuels-iMac.attlocal.net>
2020-04-02 18:09:06 -0400

    adding boiletplate to project; finished first assigment of vue course

--- vue-starter/js/scripts.js
+++ vue-starter/js/scripts.js
@@ -1,11 +1,16 @@
 new Vue({
   el: '#app',
   data: {
-    title: 'Hello, World!'
+    YOUR_NAME: 'Samuel Reid',
+    AGE: 41,
+    image: 'https://scene7.zumiez.com/is/image/zumiez/Zoom_PDP/Primitive-x-Rick-and-Morty-Pickle-Rick-Sticker-_309256-front-US.jpg'
   },
   methods: {
-    changeTitle: function(event) {
-      this.title = event.target.value;
+    ageMultiplier: function() {
+      return this.AGE * 3;
+    },
+    randomFloat: function() {
+      return Math.random();
     }
   }
 });
