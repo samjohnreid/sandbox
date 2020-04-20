4cf1ee5e704c87321bd84f2fa68b028e6993ec92
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2020-04-20 18:01:19 -0400

    two-way binding

--- vue-starter/js/scripts.js
+++ vue-starter/js/scripts.js
@@ -1,28 +1,6 @@
 new Vue({
   el: '#app',
   data: {
-    counter: 0,
-    x: 0,
-    y: 0,
-    value: 'initial value'
-  },
-  methods: {
-    increase: function(val) {
-      this.counter+= val;
-      console.log(event);
-    },
-    updateCoordinates: function(event) {
-      this.x = event.clientX;
-      this.y = event.clientY;
-    },
-    deadSpot: function(zevent) {
-      zevent.stopPropagation();
-    },
-    alert: function() {
-      console.log('weeeee key up, bro!!!');
-    },
-    updateValue: function(event) {
-      this.value = event.target.value;
-    }
+    name: 'Sammerszzzz'
   }
 });
