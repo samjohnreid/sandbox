4826219bf16a8b0cb603a78fcb2425379f8a8ca0
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2020-04-23 16:47:13 -0400

    adding loading spinner

---
+++ lottie-animations/js/scripts.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+  container: document.getElementById('app'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json' // the path to the animation json
+});
