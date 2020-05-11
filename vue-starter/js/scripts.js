f15a84e7ac7b24c4d0c64c169efdc3b29c299652
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2020-05-11 18:43:02 -0400

    finished assignment three

--- vue-starter/js/scripts.js
+++ vue-starter/js/scripts.js
@@ -1,6 +1,22 @@
 new Vue({
   el: '#app',
   data: {
-    name: 'Sammerszzzz'
+    value: 0
+  },
+  computed: {
+    result() {
+      if (this.value !== 37) {
+        return 'not there yet';
+      } else {
+        return 'done!';
+      }
+    }
+  },
+  watch: {
+    value: function() {
+      setTimeout(() => {
+        this.value = 0;
+      }, 5000);
+    }
   }
 });
