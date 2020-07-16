5c8825ae920f32351716063d98c2f959ca311a18
Samuel Reid <sam@Samuels-iMac.attlocal.net>
2020-07-15 21:55:30 -0400

    writing to db

--- firebase/js/scripts.js
+++ firebase/js/scripts.js
@@ -10,40 +10,25 @@ movies.once('value', function(snapshot) {
   console.log('Error: ', error.code);
 });
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
+function addMovieBtnClicked() {
+  
+  event.preventDefault();
 
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      return true;
-    },
-    uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
-    }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: '/',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    firebase.auth.PhoneAuthProvider.PROVIDER_ID
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
+  const title = document.querySelector('.add-movie__title').value;
+  const year = document.querySelector('.add-movie__year').value;
+  const director = document.querySelector('.add-movie__director').value;
 
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+  const newMovie = {
+    "title": title,
+    "year": year,
+    "director": director
+  };
+  
+  movies.push(newMovie, function() {
+    console.log('new movie has been added!');
+  });
+
+}
+
+const addMovieBtn = document.querySelector('.add-movie__btn');
+addMovieBtn.addEventListener('click', addMovieBtnClicked);
