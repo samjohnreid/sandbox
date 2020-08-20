e5734e2dc3ae4ad60fb9dae60394834366e46517
Samuel Reid <sam@Samuels-iMac.attlocal.net>
2020-08-19 22:51:59 -0400

    part 1 rendering and directives

---
+++ vue-intro-css-tricks/js/scripts.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: '#app',
+  data: {
+    text: 'Hello, World!',
+    items: [
+      'one',
+      'two',
+      'three',
+      'four'
+    ],
+    message: 'Hello! Weeeeeee!'
+  }
+});
