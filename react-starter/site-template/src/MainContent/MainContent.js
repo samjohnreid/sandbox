22c06a80f8127052a5288d9697cbbbc958650f4f
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2020-09-09 16:38:47 -0400

    mucking about with styled components

--- react-starter/site-template/src/MainContent/MainContent.js
+++ react-starter/site-template/src/MainContent/MainContent.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+// Styled component named StyledButton
+const StyledButton = styled.button`
+  min-width: 200px;
+  border: none;
+  font-size: 18px;
+  padding: 7px 10px;
+  background-color: ${props => props.bg === 'black' ? 'black' : 'blue'};
+`;
 
 const MainContent = () => {
   return (
     <section className="main-content">
       <h1>This is the main content area.</h1>
       <p>Blah blah blah.</p>
+      <StyledButton bg="blue" type="submit" onClick={() => alert('clicked!')}>Hello</StyledButton>
       <img src="http://placekitten.com/600/300" alt="" />
     </section>
   );
