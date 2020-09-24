ed1c671c5747d96afb646763f1b58074d685f7b4
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2020-09-24 14:55:31 -0400

    dot animations with Web Animations API

---
+++ web-animations-api/js/scripts.js
@@ -0,0 +1,48 @@
+const dot01 = document.querySelector('.dot--01');
+const dot02 = document.querySelector('.dot--02');
+const dot03 = document.querySelector('.dot--03');
+
+const dotSlide = [
+  {
+    offset: 0,
+    opacity: 0,
+    transform: 'scale(0.8)'
+  },
+  {
+    offset: 0.2,
+    opacity: 1,
+    transform: 'translateX(21px) scale(1)'
+  },
+  {
+    offset: 0.8,
+    opacity: 1,
+    transform: 'translateX(83px)'
+  },
+  {
+    offset: 1,
+    opacity: 0,
+    transform: 'translateX(104px) scale(0.8)'
+  }
+];
+
+const dotSlideOptions01 = {
+  iterations: Infinity,
+  delay: 0,
+  duration: 1200
+};
+
+const dotSlideOptions02 = {
+  iterations: Infinity,
+  delay: -400,
+  duration: 1200
+};
+
+const dotSlideOptions03 = {
+  iterations: Infinity,
+  delay: -800,
+  duration: 1200
+};
+
+dot01.animate(dotSlide, dotSlideOptions01);
+dot02.animate(dotSlide, dotSlideOptions02);
+dot03.animate(dotSlide, dotSlideOptions03);
