df09e63d866bb0bd614b79aaf1745235753d82cd
Samuel Reid <sreid2@samuels-mbp.lan>
2021-04-08 00:37:16 -0400

    adding Create and Read of CRUD

--- oscars2021/src/UserVotes/index.js
+++ oscars2021/src/UserVotes/index.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import noms from '../data/noms.js';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-const renderCategory = (forAndId, label, name, category) => {
-  return (
-    <div>
-      <label htmlFor={forAndId}>{label}:</label>
-      <select name={name} id={forAndId}>
-        <option value="">--Please Select a Winner!--</option>
-        {category.map((item, index) => {
-          return <option key={index}>{item}</option>;
-        })}
-      </select>
-    </div>
-  );
+const firebaseConfig = {
+  apiKey: "AIzaSyCN_1-Oe_PLTXfR3e4Z8zeFIyKsG-6dBrw",
+  authDomain: "oscars-2021.firebaseapp.com",
+  databaseURL: "https://oscars-2021-default-rtdb.firebaseio.com",
+  projectId: "oscars-2021",
+  storageBucket: "oscars-2021.appspot.com",
+  messagingSenderId: "861066941311",
+  appId: "1:861066941311:web:90e533e74165ed42a7804a"
 };
 
-const UserVotes = () => {
+firebase.initializeApp(firebaseConfig);
+
+// **********
+
+const votes = firebase.database().ref(`votes/dominic`);
+
+// **********
+
+const updateNom = (event) => {
+  const selectMenu = event.target.name;
+  
+  const postData = {
+    [selectMenu]: event.target.value
+  };
+
+  votes.update(postData);
+};
+
+// **********
+
+const UserVotes = () => {  
+  const [userNoms, setUserNoms] = useState();
+  
+  useEffect(() => {
+    votes.on('value', (snapshot) => {
+      setUserNoms(snapshot.val());
+    });
+  }, []);
+
+  const renderCategory = (forAndId, label, name, category) => {
+    const selected = userNoms && userNoms[name];
+    return (
+      <div>
+        <label htmlFor={forAndId}>{label}:</label>
+        <select name={name} id={forAndId} value={selected}>
+          <option>--Please Select a Winner!--</option>
+          {category.map((item, index) => {
+            return <option key={index}>{item}</option>;
+          })}
+        </select>
+      </div>
+    );
+  };
+  
   return (
     <>
-      <form id="userVotes">
+      <form id="userVotes" onChange={updateNom}>
         <ul>
           <li>{renderCategory('best-picture', 'Best Picture', 'bestPicture', noms.bestPictureNoms)}</li>
           <li>{renderCategory('best-director', 'Best Director', 'bestDirector', noms.bestDirectorNoms)}</li>
