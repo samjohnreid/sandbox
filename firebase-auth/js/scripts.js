3eaa2ec4239833401f24e755789bd51fcf270dc8
Samuel Reid <sreid2@Samuels-MacBook-Pro.local>
2021-05-27 18:36:54 -0400

    getting authentication set up

--- firebase-auth/js/scripts.js
+++ firebase-auth/js/scripts.js
@@ -40,3 +40,79 @@ database.on('value', (snapshot) => {
   const data = snapshot.val();
   samsCoolMovieFunction(data);
 });
+
+// ----------
+
+const addMovieBtnClicked = () => {
+
+  // const title = document.querySelector('.add-movie__title').value;
+  // const year = document.querySelector('.add-movie__year').value;
+  // const director = document.querySelector('.add-movie__director').value;
+
+  const title = 'New Movie';
+  const year = 2021;
+  const director = 'Sab';
+
+  const newMovie = {
+    "four": {
+      "title": title,
+      "year": year,
+      "director": director
+    }
+  };
+  
+  database.push(newMovie, function() {
+    console.log('new movie has been added!', newMovie);
+  });
+
+}
+
+const addMovieBtn = document.getElementById('addMovieButton');
+addMovieBtn.addEventListener('click', addMovieBtnClicked);
+
+// ----------
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      document.getElementById('loader').style.display = 'none';
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: '/index.html',
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
+
+// ----------
+
+const signOut = () => {
+  firebase.auth().signOut().then(() => {
+    // Sign-out successful.
+  }).catch((error) => {
+    // An error happened.
+  });
+}
+
+const signOutBtn = document.getElementById('signOutButton');
+signOutBtn.addEventListener('click', signOut);
