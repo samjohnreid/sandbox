5563d90906860160554ebe01e882ad635c825187
samjohnreid <samjohnreid@gmail.com>
2022-07-20 13:35:40 -0400

    swapping out the src of the iframe on click to start vi from beginning with mute off and controls showing

--- youtube-player-api/js/scripts.js
+++ youtube-player-api/js/scripts.js
@@ -27,3 +27,17 @@ const btnUnmute = document.getElementById('btnUnmute');
 btnUnmute.addEventListener('click', () => {
   player.unMute();  
 });
+
+const btnControls = document.getElementById('btnControls');
+btnControls.addEventListener('click', () => {
+  console.log('show controls!');
+});
+
+const btnSwapUrl = document.getElementById('btnSwapUrl');
+btnSwapUrl.addEventListener('click', () => {
+  console.log('swap URL');
+
+  const targetIframe = document.getElementById('realmax-iframe');
+  console.log('targetIframe', targetIframe);
+  targetIframe.setAttribute('src', 'https://www.youtube.com/embed/bfctwig1HUQ?enablejsapi=1&start=0&end=0&autoplay=1&loop=1&playlist=bfctwig1HUQ&version=3&rel=0');
+});
