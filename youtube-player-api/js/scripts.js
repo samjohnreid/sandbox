985e7b0348c6ee2b1f2e00b6ab9e96d5e03b0acd
samjohnreid <samjohnreid@gmail.com>
2022-07-20 12:04:04 -0400

    neatening up formatting

--- youtube-player-api/js/scripts.js
+++ youtube-player-api/js/scripts.js
@@ -1,33 +1,29 @@
-console.log('hi sam!');
-
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
-  function onYouTubeIframeAPIReady() {
-    player = new YT.Player('realmax-iframe', {
-        events: {
-          'onReady': onPlayerReady,
-        }
-    });
-  }
-  function onPlayerReady(event) {
-    document.getElementById('existing-iframe-example').style.borderColor = '#FF0000';
-    document.getElementById('existing-iframe-example').style.borderWidth = '20px';
-    console.log('hello?');
-  }
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('realmax-iframe', {
+    events: {
+      'onReady': onPlayerReady,
+    }
+  });
+}
 
-    const btnRestart = document.getElementById('btnRestart');
-    btnRestart.addEventListener('click', () => {
-        player.seekTo(0);
-    });
+function onPlayerReady(event) {
+  document.getElementById('realmax-iframe').style.borderColor = '#FF0000';
+  document.getElementById('realmax-iframe').style.borderWidth = '20px';
+  console.log('hello?');
+}
 
-    const btnUnmute = document.getElementById('btnUnmute');
-    btnUnmute.addEventListener('click', () => {
-        player.unMute();
-    });
+const btnRestart = document.getElementById('btnRestart');
+btnRestart.addEventListener('click', () => {
+  player.seekTo(0);
+});
 
-
-
+const btnUnmute = document.getElementById('btnUnmute');
+btnUnmute.addEventListener('click', () => {
+  player.unMute();  
+});
