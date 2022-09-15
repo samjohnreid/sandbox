d0a979352fc16c709bdda4f376954b13c226e390
samjohnreid <samjohnreid@gmail.com>
2022-09-15 15:13:35 -0400

    doing the promises and fetch tuts

--- go-make-things-async/js/scripts.js
+++ go-make-things-async/js/scripts.js
@@ -1,1 +1,68 @@
-// sandbox js
+let count = new Promise((resolve, reject) => {
+	resolve(1);
+});
+
+count.then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+}).then((num) => {
+	console.log(num);
+	return num + 1;
+});
+
+let question = new Promise((resolve, reject) => {
+	resolve(42);
+});
+
+setTimeout(() => {
+	question.then((answer) => {
+		console.log(answer);
+	});
+}, 5000);
+
+// -----------------------------------------------
+
+fetch('https://jsonplaceholder.typicode.com/postsssss').then((response) => {
+	if (response.ok) {
+		return response.json();
+	}
+
+	throw response.status;
+}).then((data) => {
+	console.log(data);
+}).catch((error) => {
+	console.warn('Oh no! There was an error.', error);
+});
+
+// -----------------------------------------------
+
+let counterNumContainer = document.getElementById('counter');
+let counterNum = parseInt(counterNumContainer.innerText);
+setInterval(() => {
+	counterNum = counterNum + 1;
+	counterNumContainer.innerText = counterNum;
+}, 1000)
