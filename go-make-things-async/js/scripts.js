36612a21de122d6077983b7866a3da04abd4a145
samjohnreid <samjohnreid@gmail.com>
2022-09-20 12:37:30 -0400

    async await

--- go-make-things-async/js/scripts.js
+++ go-make-things-async/js/scripts.js
@@ -46,7 +46,7 @@ setTimeout(() => {
 
 // -----------------------------------------------
 
-fetch('https://jsonplaceholder.typicode.com/postsssss').then((response) => {
+fetch('https://ghibliapi.herokuapp.com/films').then((response) => {
 	if (response.ok) {
 		return response.json();
 	}
@@ -66,3 +66,70 @@ setInterval(() => {
 	counterNum = counterNum + 1;
 	counterNumContainer.innerText = counterNum;
 }, 1000)
+
+// -----------------------------------------------
+
+async function traditionalFn () {
+	await fetch('https://jsonplaceholder.typicode.com/posts/').then(function (response) {
+		return response.json();
+	}).then(function (data) {
+		console.log('Traditional Fetch', data);
+	});
+	console.log('Traditional Message');
+}
+traditionalFn();
+
+
+async function getTheAnswer() {
+	return 42;
+}
+
+const answer = getTheAnswer();
+
+answer.then((data) => {
+	console.log('huh?', data);
+});
+
+/**
+ * Get an article by its ID
+ * @param  {Integer} id The article ID
+ */
+ async function getArticleByID (id) {
+	try {
+		let response = await fetch(`https://ghibliapi.herokuapp.com/films/${id}`);
+		
+		// If the call failed, throw an error
+		if (!response.ok) {
+			throw 'Something went wrong.';
+		}
+
+		// Otherwise, get the post JSON
+		let data = await response.json();
+
+		// Log the data to the console
+		console.log('ghibli movie: ', data);
+	} catch (error) {
+		console.warn('ERROR: ', error);
+	}
+}
+
+// Get the article with an ID of 3
+getArticleByID('dc2e6bd1-8156-4886-adff-b39e6043af0c');
+
+
+
+
+
+
+
+
+
+const fetchGhibli = () => {
+	fetch('https://ghibliapi.herokuapp.com/films').then((response) => {
+		return response.json();
+	}).then((data) => {
+		console.log('Ghibli!', data);
+	});
+}
+
+fetchGhibli();
