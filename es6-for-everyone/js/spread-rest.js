31c33bc8c914be6d2a479b27cafdd3f9176b423d
samjohnreid <samjohnreid@gmail.com>
2022-12-05 17:02:09 -0500

    spread exercise

--- es6-for-everyone/js/spread-rest.js
+++ es6-for-everyone/js/spread-rest.js
@@ -10,7 +10,6 @@ console.log('heroes', heroes);
 
 const marvelCopy = [...marvel];
 
-
 console.log('marvelCopy: ', marvelCopy);
 
 marvelCopy[0] = 'Superman';
@@ -18,3 +17,20 @@ marvelCopy[0] = 'Superman';
 console.log('marvelCopy[0]: ', marvelCopy[0]);
 
 console.log('marvel[0]: ', marvel[0]);
+
+console.log('// ----------------------------------------------');
+
+// Exercise
+
+const wordContainer = document.querySelector('.jump');
+const word = document.querySelector('.jump').innerText;
+
+const wordArray = [...word];
+
+const newWordArray = wordArray.map((letter) => {
+    return `<span>${letter}</span>`;
+}).join('');
+
+wordContainer.innerHTML = newWordArray;
+
+console.log('newWordArray', newWordArray);
