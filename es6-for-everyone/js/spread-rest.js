2465cf8d0f47ab6e1866850940bdf5b96aa509bf
samjohnreid <samjohnreid@gmail.com>
2022-12-05 15:34:52 -0500

    spread operator

--- es6-for-everyone/js/spread-rest.js
+++ es6-for-everyone/js/spread-rest.js
@@ -1,1 +1,20 @@
-alert('woot');
+const marvel = ['Spider-Man', 'Iron Man', 'Thor']
+const dc = ['Batman', 'Aquaman', 'Wonder Woman'];
+
+const heroes = [...marvel, 'Flash', ...dc]
+
+console.log('marvel', marvel);
+console.log('dc', dc);
+
+console.log('heroes', heroes);
+
+const marvelCopy = [...marvel];
+
+
+console.log('marvelCopy: ', marvelCopy);
+
+marvelCopy[0] = 'Superman';
+
+console.log('marvelCopy[0]: ', marvelCopy[0]);
+
+console.log('marvel[0]: ', marvel[0]);
