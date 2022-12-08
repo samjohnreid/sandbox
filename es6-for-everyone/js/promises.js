0ae1b6277c578b352e2be1103a5e60d56913adb4
samjohnreid <samjohnreid@gmail.com>
2022-12-08 13:52:22 -0500

    promise exercise

--- es6-for-everyone/js/promises.js
+++ es6-for-everyone/js/promises.js
@@ -5,3 +5,13 @@ pokemonsPromise
     .then(data => console.log('second data:', data))
     .catch(err => console.error('Error!', err))
 ;
+
+const p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Sam is cool!');
+    }, 1000);
+});
+
+p.then((data) => {
+    console.log('data:', data);
+});
