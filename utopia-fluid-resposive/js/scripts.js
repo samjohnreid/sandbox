4e4511d4c86a866806c6bcef6c96e3bb837114f8
samjohnreid <samjohnreid@gmail.com>
2024-01-09 09:38:11 -0500

    outputting viewport width for testing purposes

--- utopia-fluid-resposive/js/scripts.js
+++ utopia-fluid-resposive/js/scripts.js
@@ -1,1 +1,7 @@
-// sandbox js
+const viewportWidthContainer = document.getElementById('viewportWidth');
+
+const getViewportWidth = () => viewportWidthContainer.innerText = window.innerWidth;
+
+window.addEventListener('resize', () => getViewportWidth());
+
+getViewportWidth();
