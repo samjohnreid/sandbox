8fd3705a4911b02c10a4f7732877b14a864cbc55
samjohnreid <samjohnreid@gmail.com>
2024-06-02 21:10:10 -0400

    adding webgl compatibility check

--- three-js/js/scripts.js
+++ three-js/js/scripts.js
@@ -1,11 +1,12 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
+
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -21,5 +22,17 @@ function animate() {
 	cube.rotation.y += 0.01;
 
 	renderer.render( scene, camera );
+
+}
+
+if ( WebGL.isWebGLAvailable() ) {
+
+	// Initiate function or other initializations here
+	renderer.setAnimationLoop( animate );
+
+} else {
+
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
 
 }
