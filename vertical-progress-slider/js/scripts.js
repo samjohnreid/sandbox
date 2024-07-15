5c20c02ff6055b7501544aa88a00fc379066fa87
samjohnreid <samjohnreid@gmail.com>
2024-07-15 15:18:58 -0400

    getting the scroll progress indicator firing

--- vertical-progress-slider/js/scripts.js
+++ vertical-progress-slider/js/scripts.js
@@ -1,1 +1,13 @@
-// sandbox js
+const scrollArea = document.querySelector('.vps__content-scroll-area');
+const scrollbar = document.querySelector('.vps__scrollbar');
+
+const progressIndicator = () => {
+	const scrollPos = scrollArea.scrollTop;
+	const scrollAreaHeight = scrollArea.scrollHeight - scrollArea.offsetHeight;
+	const scrolled = (scrollPos / scrollAreaHeight) * 100;
+	scrollbar.style.height = `${scrolled}%`;
+}
+
+scrollArea.addEventListener('scroll', () => {
+	progressIndicator();
+});
