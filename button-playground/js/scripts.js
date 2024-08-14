bcb959fe8c4675ab3ab530536be760b8668ae67a
samjohnreid <samjohnreid@gmail.com>
2024-08-14 11:43:01 -0400

    getting animations popping on mouse in and out

--- button-playground/js/scripts.js
+++ button-playground/js/scripts.js
@@ -1,1 +1,11 @@
-// sandbox js
+const button = document.querySelector('.button-1');
+
+button.addEventListener('mouseenter', (event) => {
+	event.target.classList.remove('button-test-out');
+	event.target.classList.add('button-test-in');
+});
+
+button.addEventListener('mouseleave', (event) => {
+	event.target.classList.remove('button-test-in');
+	event.target.classList.add('button-test-out');
+});
