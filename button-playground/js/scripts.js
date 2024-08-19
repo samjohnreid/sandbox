4a33465adf6c70349beb0d910bad79b0cbe0ece9
samjohnreid <samjohnreid@gmail.com>
2024-08-19 18:36:06 -0400

    adding alt button style

--- button-playground/js/scripts.js
+++ button-playground/js/scripts.js
@@ -1,16 +1,34 @@
 let root = document.documentElement;
 
-const button = document.querySelector('.button-1');
+// **************************************************
 
-button.addEventListener('mouseenter', (event) => {
-	let btnWidth = (event.target.offsetWidth - 22) * 2;
+const button1 = document.querySelector('.button-1');
+const button2 = document.querySelector('.button-2');
+
+// **************************************************
+
+button1.addEventListener('mouseenter', (event) => {
+	const btnWidth = (event.target.offsetWidth - 21) * 2;
 	root.style.setProperty('--btn-width', `${btnWidth}px`);
 	
 	event.target.classList.remove('button-test-out');
 	event.target.classList.add('button-test-in');
 });
 
-button.addEventListener('mouseleave', (event) => {
+button1.addEventListener('mouseleave', (event) => {
+	event.target.classList.remove('button-test-in');
+	event.target.classList.add('button-test-out');
+});
+
+button2.addEventListener('mouseenter', (event) => {
+	const btnWidth = event.target.offsetWidth - 38;
+	root.style.setProperty('--btn-width', `${btnWidth}px`);
+	
+	event.target.classList.remove('button-test-out');
+	event.target.classList.add('button-test-in');
+});
+
+button2.addEventListener('mouseleave', (event) => {
 	event.target.classList.remove('button-test-in');
 	event.target.classList.add('button-test-out');
 });
