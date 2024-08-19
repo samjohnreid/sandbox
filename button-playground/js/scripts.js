062f3aac53386c3e42ae43f94f0a208f9b4d8a0e
samjohnreid <samjohnreid@gmail.com>
2024-08-19 16:45:03 -0400

    passing in width of button dynamically

--- button-playground/js/scripts.js
+++ button-playground/js/scripts.js
@@ -1,6 +1,11 @@
+let root = document.documentElement;
+
 const button = document.querySelector('.button-1');
 
 button.addEventListener('mouseenter', (event) => {
+	let btnWidth = (event.target.offsetWidth - 22) * 2;
+	root.style.setProperty('--btn-width', `${btnWidth}px`);
+	
 	event.target.classList.remove('button-test-out');
 	event.target.classList.add('button-test-in');
 });
