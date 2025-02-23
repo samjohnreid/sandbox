a30e62fb80f0194a9354f979fb8e53b0d334065b
samjohnreid <samjohnreid@gmail.com>
2025-02-22 22:20:50 -0500

    animating objects with webgl

--- webgl/js/scripts.js
+++ webgl/js/scripts.js
@@ -1,6 +1,9 @@
 import { initBuffers } from "./init-buffers.js";
 import { drawScene } from "./draw-scene.js";
 
+let squareRotation = 0.0;
+let deltaTime = 0;
+
 main();
 
 function main() {
@@ -112,6 +115,19 @@ function main() {
 	// Here's where we call the routine that builds all the objects we'll be drawing.
 	const buffers = initBuffers(gl);
 
-	// Draw the scene
-	drawScene(gl, programInfo, buffers);
+	let then = 0;
+
+	// Draw the scene repeatedly
+	function render(now) {
+		now *= 0.001; // convert to seconds
+		deltaTime = now - then;
+		then = now;
+
+		drawScene(gl, programInfo, buffers, squareRotation);
+		squareRotation += deltaTime;
+
+		requestAnimationFrame(render);
+	}
+	requestAnimationFrame(render);
+
 }
